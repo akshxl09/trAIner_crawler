672a5a6f4b3214fa554fae33c82e5374dd29d5e6
Taebo Won <asun291@naver.com>
2022-10-13 22:02:33 +0900

    feat: hot user status 구하기

--- app/hot_user.py
+++ app/hot_user.py
@@ -4,52 +4,79 @@ from pymongo import MongoClient
 from matplotlib import pyplot as plt
 from matplotlib import ticker as ticker
 
+class Crawler:
+    def __init__(self, database):
+        self.db = database['Jarvis']
 
-def get_hot_user(database: MongoClient):
-    db = database['Jarvis']
-    col = db['interact']
-    users = col.distinct('userId')
-    #전체 유저 수
-    print(len(users))
 
-    interact = list(col.aggregate([
-        {
-            "$group": {
-                "_id": "$userId",
-                "count": {"$sum": 1}
+    def get_appropriate_count(self):
+        col = self.db['interact']
+        users = col.distinct('userId')
+        #전체 유저 수
+        print(len(users))
+
+        interact = list(col.aggregate([
+            {
+                "$group": {
+                    "_id": "$userId",
+                    "count": {"$sum": 1}
+                }
             }
-        }
-    ]))
+        ]))
 
-    avg = list(col.aggregate([
-        {
-            "$group": {
-                "_id": "$userId",
-                "count": {"$sum": 1}
+        avg = list(col.aggregate([
+            {
+                "$group": {
+                    "_id": "$userId",
+                    "count": {"$sum": 1}
+                }
+            },
+            {
+                "$match": {
+                    "count": {"$gt": 400}
+                }
             }
-        },
-        {
-            "$match": {
-                "count": {"$gt": 400}
+        ]))
+        avg = sorted(avg, key=lambda x: x['count'])
+
+        graph = sorted(x['count'] for x in interact)
+        plt.figure(figsize=(25, 10))
+        plt.plot(graph)
+        plt.xticks(np.arange(0, 100000, step=5000))
+        plt.yticks(np.arange(0, 10000, step=200))
+        plt.grid(True)
+        plt.show()
+
+
+    def get_hot_user(self):
+        interact = self.db['interact']
+        avg = list(interact.aggregate([
+            {
+                "$group": {
+                    "_id": "$userId",
+                    "count": {"$sum": 1}
+                }
+            },
+            {
+                "$match": {
+                    "count": {"$gt": 400}
+                }
             }
-        }
-    ]))
-    avg = sorted(avg, key=lambda x: x['count'])
-
-
-    graph = sorted(x['count'] for x in interact)
-    plt.figure(figsize=(25, 10))
-    plt.plot(graph)
-    plt.xticks(np.arange(0, 100000, step=5000))
-    plt.yticks(np.arange(0, 10000, step=200))
-    plt.grid(True)
-    plt.show()
+        ]))
+        avg = sorted(avg, key=lambda x: x['count'])
+        users = []
+        for user in avg:
+            users.append({
+                'userId': user['_id']
+            })
+        self.db['user'].insert_many(users)
 
 
 if __name__ == '__main__':
     from dotenv import load_dotenv
     load_dotenv(verbose=True)
 
-    get_hot_user(
+    crawler = Crawler(
         database=MongoClient(os.environ['REAL_DB'])
     )
+    crawler.get_hot_user()
--- app/interact.py
+++ app/interact.py
@@ -15,7 +15,7 @@ class Crawler:
         self.size = size
 
 
-    def get_problem(self):
+    def get_status(self):
         problems = list(self.local['solved_ac']['problem'].find())
         for data in problems:
             cur = self.prod['Jarvis']['interact_config'].find_one({'problemId': data['problemId']})
@@ -92,6 +92,82 @@ class Crawler:
                 pbar.close()
 
 
+    def get_hot_user_status(self):
+        users = list(self.prod['Jarvis']['user'].find())
+        total_user_cnt = len(users)
+        user_cnt = 0
+        for user in users:
+            cur = self.prod['Jarvis']['user'].find_one({'userId': user['userId']})
+            if 'top' in cur.keys():
+                count = cur['count']
+                end = cur['end']
+                url = f"https://www.acmicpc.net/status?user_id={cur['userId']}&top={cur['top']}"
+            else:
+                count = 0
+                end = False
+                url = f"https://www.acmicpc.net/status?user_id={user['userId']}"
+            
+            user_cnt += 1
+            print(f"전체 {total_user_cnt}명의 유저 중 {user_cnt}번 째 유저 '{user['userId']}' 수집중")
+            while not end:
+                sleep(random.uniform(0.5, 2))
+                response = requests.get(url, headers=self.headers)
+                soup = BeautifulSoup(response.text, "html.parser")
+
+                #다음 url 확인
+                next = soup.select_one('#next_page', href=True)
+                if next:
+                    idx = next['href'].find('top=') + 4
+                    next_page_submit_id = next['href'][idx:]
+                    end = False
+                else:
+                    next_page_submit_id = ''
+                    end = True
+                url = f"https://www.acmicpc.net/status?user_id={user['userId']}&top={next_page_submit_id}"
+                table = soup.select('#status-table tbody tr')
+                #페이지당 최대 20개의 제출기록
+                for tr in table:
+                    status = []
+                    tds = tr.select('td')
+                    for td in tds:
+                        time = td.select_one('a')
+                        if time and time.has_attr("data-timestamp"):
+                            status.append(time['data-timestamp'])
+                        else:
+                            status.append(td.text)
+                    """
+                    모종의 이유로 크롤링이 멈췄다가 다시 재개될 경우,
+                    이전에 수집했던 부분 다음부터 수집하도록 조건 추가
+                    """
+                    if 'top' in cur.keys() and int(cur['currentSubmitId']) <= int(status[0]):
+                        continue
+                    self.prod['Jarvis']['interact'].update_one(
+                        {'submitId': status[0]},
+                        { '$set': {
+                                'userId': status[1],
+                                'problemId': status[2],
+                                'result': status[3],
+                                'memory': status[4],
+                                'time': status[5],
+                                'language': status[6],
+                                'codeLength': status[7],
+                                'timestamp': status[8]
+                            }
+                        }
+                    )
+                    count += 1
+                    self.prod['Jarvis']['user'].update_one(
+                        {'userId': user['userId']},
+                        { '$set': {
+                                'currentSubmitId': status[0],
+                                'top': next_page_submit_id,
+                                'count': count,
+                                'end': end
+                            }
+                        }, upsert=True
+                    )
+
+
 if __name__ == '__main__':
     from dotenv import load_dotenv
     load_dotenv(verbose=True)
@@ -104,17 +180,18 @@ if __name__ == '__main__':
         prod_database=MongoClient(os.environ['REAL_DB']),
         size=1000
     )
+    #crawler.get_status()
+    crawler.get_hot_user_status()
 
-    process_time = time()
-    crawler.get_problem()
-    process_time = time() - process_time
 
-    print(process_time)
+
 
 """
 백준 문제 url: https://www.acmicpc.net/problem/1000
 백준 특정 문제 통계 url: https://www.acmicpc.net/problem/status/1000
 백준 전체 통계 url: https://www.acmicpc.net/status?problem_id=1264
+
+백준 젠처 통계 url: https://www.acmicpc.net/status?user_id=nmp9981&top=49749180
 
 
 문제 채점현황 맨 마지막 url: https://www.acmicpc.net/status?problem_id=1845&from_problem=1&top=16738296
